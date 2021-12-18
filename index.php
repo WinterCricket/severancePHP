6fe5b52291e0486fb5a225dd15bc4b90f13fd4fe
wintercricket <hodhodhod2003@gmail.com>
2021-12-18 17:24:03 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Severance PHP: Index File</title>
+</head>
+<body>
+  <h1>Index File for Severance PHP</h1>
+  <h2>HTML page</h2>
+  <?php
+  echo "This is the php course";
+$x = 4 * 44;
+echo "List the number ".$x." times by morning!";
+echo "Then jump toward the morning moon.\n";
+echo "Trying hard to do this thing.";
+ 
+
+?>
+<p>This was written in HTML, not in PHP.</p>
+</body>
+</html>
