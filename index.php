934c79773e41fa66bbc42f9175e2aea9f91162ad
wintercricket <hodhodhod2003@gmail.com>
2021-12-18 21:46:31 -0600

    deposits via for loop

--- index.php
+++ index.php
@@ -14,17 +14,20 @@
   $x = 4 * 44;
   echo "List the number ".$x." times by morning!\n";
   echo "Then jump toward the morning moon.\n";
-  
   $money = 7700;
   while($money > 400){
     print "<p>You're down to $".$money." in your account.💰</p>\n";
-    $money = $money - 5200;
+    $money = $money - 400;
   }
 
+  
+ for($deposit = 0; $deposit <= 15; $deposit++){
+    echo "$deposit of 200 is $". $deposit * 200;
+    echo "\n";
+  }
  
 
 ?>
 <p>This was written in HTML, not in PHP.</p>
 </body>
 </html>
-
